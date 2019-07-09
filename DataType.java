926c33ce0cbbbc01b76969cb12e732cb8a692037
madhuriikke <madhuriikke2@gmail.com>
2019-07-09 13:47:16 +0530

    madhuri done

---
+++ DataType.java
@@ -0,0 +1,49 @@
+
+public class DataType {
+	
+	public static void main(String[]args) {
+	
+		int a;//Declaration 
+		int i=123456;//Initialization-->stores integer value
+		float f=10234f;//stores decimal values
+		long l=1234567853467432l;//Stores integer values more than 10 digits
+		double d=1.234567896544;
+		boolean b=false;
+		char c='s';//stores single character
+		String s="This is java tutorial";//stores multiple characters or sentences
+		
+	
+
+ DataType dt;
+ //Camel casing
+ int batmanBeging;
+ int theDarkKnight;
+ int theDarkKnightRises;
+ 
+ //String Concatenation
+ String abc=new String();
+ abc="Hello";
+ String abc1="Hello";//Both abc and abc1 are same
+ 
+ String s1="Hello";
+ String s2="Way";
+ String s3="Automation";
+ String s4=s1 +" "+s2+" "+ s3;
+ System.out.println(s4);
+ System.out.println(s1+s2+s3);
+ 
+ 
+ System.out.println("----------");
+ //String + String-->String;
+ //int + int-->int;
+ int num1=10;
+ int num2=5;
+ System.out.println(num1+num2);
+ System.out.println(num1+num2+"after");
+ System.out.println("befoer "+(num1+num2)+"  after");
+ 
+ 
+
+	}
+}
+ 
